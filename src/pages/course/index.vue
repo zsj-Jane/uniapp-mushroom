8d7cc9e5be22d4c074c034dd6618b8e05e494a80
Jane <582194643@qq.com>
2020-04-07 17:21:40 +0800

    项目起步-图片资源+删除无用的初始化文件

---
+++ src/pages/course/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <view>
+    <text>课程</text>
+  </view>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+    data(){
+
+    },
+    onLoad(){
+
+    },
+    methods:{}
+});
+</script>
+
+<style>
+</style>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,6 +1,5 @@
 <template>
 	<view class="content">
-        <image class="logo" src="../../static/logo.png"></image>
 		<view>
             <text class="title">{{title}}</text>
         </view>
