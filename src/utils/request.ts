97e37ff58c381d3c1c33918ef1306b10c1f4a03f
Jane <582194643@qq.com>
2020-04-10 10:46:37 +0800

    封装请求方法

---
+++ src/utils/request.ts
@@ -0,0 +1,40 @@
+// 基地址
+const BASEURL: string = 'http://localhost:3000/api/';
+// 创建一个请求函数
+const request = ({ url = '', method = 'GET', data = {}, tipName = '正在加载中' }) => {
+    return new Promise((resolve, reject) => {
+        // 显示 正在加载中
+        uni.showLoading({
+            title: tipName
+        });
+        const header = {
+            Authorization: ''
+        };
+        if (uni.getStorageSync('my_token')) {
+            header.Authorization = uni.getStorageSync('my_token');
+        } else {
+
+        }
+        // 发送请求
+        uni.request({
+            url: `${BASEURL}${url}`,
+            method,
+            data,
+            header: {
+                'custom-header': 'hello' //自定义请求头信息
+            },
+            success: (res) => {
+                resolve(res);
+            },
+            fail: (err) => {
+                reject(err);
+            },
+            complete: () => {
+                // 关闭提示
+                uni.hideLoading();
+            }
+        });
+    })
+}
+// 暴露request请求方法
+export default request
