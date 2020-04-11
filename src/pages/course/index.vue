8ee6bd58ab95d0e8f90a7c9c6da4f56828251d40
Jane <582194643@qq.com>
2020-04-11 16:52:51 +0800

    课程页面-页面渲染

--- src/pages/course/index.vue
+++ src/pages/course/index.vue
@@ -1,21 +1,32 @@
 <template>
-  <view>
-    <text>课程</text>
-  </view>
+  <courseList :courses="courses"></courseList>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
+import request from "../../utils/request";
+import courseList from "../../components/course-list/index.vue";
 export default Vue.extend({
-    data(){
-
-    },
-    onLoad(){
-
-    },
-    methods:{}
+  components: {
+    courseList
+  },
+  data() {
+    return {
+      courses: []
+    };
+  },
+  onLoad() {
+    this.getCoursesData();
+  },
+  methods: {
+    // 获取课程列表数据
+    async getCoursesData() {
+      let res: any = await request({ url: "course/list" });
+      console.log(res.data);
+      if (res.data.status === 0) {
+        this.courses = res.data.message;
+      }
+    }
+  }
 });
 </script>
-
-<style>
-</style>
